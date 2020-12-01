7c799020545fc377e3f5d51f8ebcd89ed707fa6b
adamsyah <fadillahadam11@gmail.com>
2020-12-01 17:09:51 +0700

    Completing the 1st assignment

--- ros_essentials_cpp/src/assignment/turtlesim_pose.py
+++ ros_essentials_cpp/src/assignment/turtlesim_pose.py
@@ -3,22 +3,17 @@
 import rospy
 from turtlesim.msg import Pose
 
-
 def poseCallback(pose_message):
-    print "pose callback"
-    print ('x = {}'.format(pose_message.x)) #new in python 3
-    print ('y = %f' %pose_message.y) #used in python 2
-    print ('yaw = {}'.format(pose_message.theta)) #new in python 3
-
-
-if __name__ == '__main__':
+    print("pose callback")
+    print(f"x = {pose_message.x}")
+    print(f"y = {pose_message.y}")
+    print(f"yaw = {pose_message.theta}")
+    
+if __name__ == "__main__":
     try:
-        
-        rospy.init_node('turtlesim_motion_pose', anonymous=True)
-        
-        position_topic = "/turtle1/pose"
-        pose_subscriber = rospy.Subscriber(position_topic, Pose, poseCallback) 
+        rospy.init_node("turtlesim_motion_pose", anonymous=True)
+        rospy.Subscriber("turtle1/pose", Pose, poseCallback)
         rospy.spin()
-       
+        
     except rospy.ROSInterruptException:
         rospy.loginfo("node terminated.")
